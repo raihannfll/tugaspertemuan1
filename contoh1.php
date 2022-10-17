9809c8c2ae05f346640ee4acf5ca01e4bf51cce4
raihannfll <113744628+raihannfll@users.noreply.github.com>
2022-10-17 16:58:01 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller {
+
+	
+	public function index()
+	{
+		echo "<h1>perkenalkan</h1>";
+        echo"Nama Saya Raihan Naufal 
+            saya tinggal di daerah ciputat
+            hoby saya bulu tangkis";
+	}
+}
